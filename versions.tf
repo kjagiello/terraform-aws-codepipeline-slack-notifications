595ad3ae7d656be40c3b7171a5493772c9805e8a
Krzysztof Jagiello <me@kjagiello.com>
2020-10-01 17:11:27 +0200

    Add support for Terraform 0.13

---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 1.3"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+  required_version = ">= 0.12"
+}
