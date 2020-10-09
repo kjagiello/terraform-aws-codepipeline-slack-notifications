64eac4399197f2bef0c7256c017c965d80f99255
Markus Rydh <markus.rydh@grouptalk.com>
2020-10-09 15:51:04 +0200

    Updated dependency for terraform 0.13

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "default_label" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.4.0"
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.5.0"
   namespace  = var.namespace
   stage      = var.stage
   name       = var.name
