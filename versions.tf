37b1e1088292cac53c6ac3d60d8fe16094a91cb2
Markus Rydh <markus.rydh@grouptalk.com>
2020-10-09 16:04:34 +0200

    Updated provider version dependencies to be more allowing.

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     archive = {
       source  = "hashicorp/archive"
-      version = "~> 1.3"
+      version = ">= 1.3"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2.70"
+      version = ">= 2.70"
     }
   }
   required_version = ">= 0.12"
