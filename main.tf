790c58bd02612152984d57bd1030c908f9782a43
Krzysztof Jagiello <me@kjagiello.com>
2021-04-18 14:13:15 +0200

    fixup! Switch to terraform-null-label

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "default_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.24.1"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.22.0"
   namespace  = var.namespace
   stage      = var.stage
   name       = var.name
